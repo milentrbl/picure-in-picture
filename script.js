e100bbb9263628c82bdb91692bcf9e049706f428
Milena Demirtshyan <milenademirtshyan04@gmail.com>
2022-10-14 20:42:20 +0400

    added everything

---
+++ script.js
@@ -0,0 +1,27 @@
+const videoElement = document.getElementById('video')
+const button = document.getElementById('button')
+
+console.log(window.navigator);
+
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () =>{
+            videoElement.play();
+        }
+    } catch (error) {
+        
+    }
+}
+button.addEventListener('click',async()=>{
+    //Disable button
+    button.disabled = true;
+    // start picture in picture
+    await videoElement.requestPictureInPicture();
+    // Reset button
+    button.disabled = false
+})
+
+// On load
+selectMediaStream()
